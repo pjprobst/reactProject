3ce8e00b7e077e4f9f468bebb67e2b1ab94dacc2
pjprobst <111828284+pjprobst@users.noreply.github.com>
2023-12-08 11:10:38 -0600

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,17 @@
+import React, { useStae } from 'react'
+import './App.css'
 function App() {
+  const [value, setValue] = useState('')
   return (
-    <div className="App">
-      Hello, World
+    <div className="container">
+      <div className="calculator">
+        <Form action-"">
+          <div>
+            <input type="text" />
+        </form>
+      </div>
     </div>
-  );
+    )
 }
 
 export default App;
